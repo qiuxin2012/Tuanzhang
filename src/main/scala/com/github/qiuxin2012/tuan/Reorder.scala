91c54073be1cdaa8abe75247f7c4da75804764ad
Xin Qiu <qiuxin2012@users.noreply.github.com>
2022-08-12 15:33:56 +0800

    Update Reorder.scala

--- src/main/scala/com/github/qiuxin2012/tuan/Reorder.scala
+++ src/main/scala/com/github/qiuxin2012/tuan/Reorder.scala
@@ -1,19 +1,17 @@
-package com.github.qiuxin2012.tuan
-
-
-import org.apache.poi.ss.usermodel.{Row, Sheet}
+import org.apache.poi.ss.usermodel.{BorderStyle, Row, Sheet}
 
 import java.io.{File, FileInputStream}
 import java.io.FileOutputStream
 import java.io.IOException
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.xssf.usermodel.{XSSFCellStyle, XSSFWorkbook}
 
+import java.awt.Color
 import scala.collection.mutable.ArrayBuffer
 
 object Reorder {
   def main(args: Array[String]): Unit = {
-    val filePath = args(0)
-//    val filePath = "C:\\Users\\xinqiu\\Downloads\\订单20_50_50.xlsx.xlsx"
+//    val filePath = args(0)
+    val filePath = "C:\\Users\\xinqiu\\Downloads\\订单15_28_40.xlsx"
     val fs = new FileInputStream(filePath)
     val xssfWorkbook: XSSFWorkbook = new XSSFWorkbook(fs)
     val order = ArrayBuffer[Order]()
@@ -46,15 +44,21 @@ object Reorder {
         no.toInt, room))
     }
     val orders = order.toArray.sortBy(_.room).sortBy(_.no)
+    val names = orders.map(_.name).distinct
+    println(names.mkString("\n"))
+      //.filter(v => v.item == "悦鲜活牛奶+简醇酸奶套装(牛奶950ml*6瓶/酸奶150克*16袋)" || v.item == "白小纯常温奶(180克*16袋)")
     var out: FileOutputStream = null
     try { // 获取总列数
-      val outputPath = args(1)
-//      val outputPath = new File("C:\\Users\\xinqiu\\Downloads\\out.xlsx")
+//      val outputPath = args(1)
+      val outputPath = new File("C:\\Users\\xinqiu\\Downloads\\out.xlsx")
       val workBook = new XSSFWorkbook()
-      val partSplit = Array(0, 55, 79, 97, 128)
+      val cellStyle = workBook.createCellStyle()
+      //val partSplit = Array(0, 55, 79, 97, 128)
+      val partSplit = Array(0, 128)
+//      val partSplit = Array(0, 59, 128)
       (1 until partSplit.length).foreach{part =>
         writeToSheet(orders.filter(o => o.no >= (partSplit(part-1)+1) && o.no <= partSplit(part)),
-          workBook.createSheet(s"${partSplit(part-1)+1}-${partSplit(part)}号"))
+          workBook.createSheet(s"${partSplit(part-1)+1}-${partSplit(part)}号"), cellStyle)
       }
 
       // 创建文件输出流，输出电子表格：这个必须有，否则你在sheet上做的任何操作都不会有效
@@ -73,32 +77,46 @@ object Reorder {
     }
   }
 
-  def writeToSheet(orders: Array[Order], sheet: Sheet): Unit = {
+  def writeToSheet(orders: Array[Order], sheet: Sheet, cellStyle: XSSFCellStyle): Unit = {
 
     var noItems = scala.collection.mutable.Map(orders.map(_.item).distinct.map(i => (i, 0)): _*)
     val partItems = noItems.clone()
     var currentNo = orders(0).no
-    val title = Array("名字","商品","数量","电话","楼栋","房号")
+    val title = Array("名字","商品","数量","楼栋","房号","电话")
     set(sheet.createRow(0), title)
     var i = 0
     var offset = 1
+    import org.apache.poi.xssf.usermodel.XSSFCellStyle
+    var orderName = orders(0).name
     while(i < orders.length) {
       val order = orders(i)
       noItems(order.item) = noItems(order.item) + order.num
       partItems(order.item) = partItems(order.item) + order.num
+
       val row = sheet.createRow(i + offset)
-      set(row, order)
+      if (orderName != order.name) {
+        cellStyle.setBorderBottom(BorderStyle.HAIR)
+        setWithBorder(row, order, cellStyle)
+      } else {
+        cellStyle.setBorderBottom(BorderStyle.NONE)
+        setWithBorder(row, order, cellStyle)
+      }
       if (i+1 == orders.length || order.no != orders(i+1).no) {
         offset += 1
-        set(sheet.createRow(i + offset), Array(s"${order.no}号")
-          ++ noItems.flatMap(v => Array(s"${v._1}", s"${v._2}份")))
+        // set(sheet.createRow(i + offset), Array(s"${order.no}号")
+        //   ++ noItems.filter(_._2 != 0).flatMap(v => Array(s"${v._1}", s"${v._2}份")))
+//        set(sheet.createRow(i + offset), Array(s"${order.no}号")
+//          ++ noItems.filter(_._2 != 0).flatMap(v => Array(s"${v._1} ${v._2}份")))
+//        offset += 1
         noItems = scala.collection.mutable.Map(orders.map(_.item).distinct.map(i => (i, 0)): _*)
         currentNo = orders(i).no
       }
       i += 1
     }
-    set(sheet.createRow(i + offset + 1), Array(sheet.getSheetName)
-      ++ partItems.flatMap(v => Array(s"${v._1}", s"${v._2}份")))
+    // set(sheet.createRow(i + offset + 1), Array("总"+sheet.getSheetName)
+    //   ++ partItems.flatMap(v => Array(s"${v._1}", s"${v._2}份")))
+    set(sheet.createRow(i + offset + 1), Array("总"+sheet.getSheetName)
+      ++ partItems.flatMap(v => Array(s"${v._1} ${v._2}份")))
   }
 
   def set(r: Row, o: Order): Unit = {
@@ -109,6 +127,28 @@ object Reorder {
     r.createCell(3).setCellValue(o.tel)
     r.createCell(4).setCellValue(o.no)
     r.createCell(5).setCellValue(o.room)
+  }
+
+  def setWithBorder(r: Row, o: Order, cellStyle: XSSFCellStyle): Unit = {
+    //    println(r.getRowNum)
+    val r0 = r.createCell(0)
+    r0.setCellStyle(cellStyle)
+    r0.setCellValue(o.name)
+    val r1 = r.createCell(1)
+    r1.setCellValue(o.item)
+    r1.setCellStyle(cellStyle)
+    val r2 = r.createCell(2)
+    r2.setCellValue(o.num)
+    r2.setCellStyle(cellStyle)
+    val r3 = r.createCell(3)
+    r3.setCellValue(o.no)
+    r3.setCellStyle(cellStyle)
+    val r4 = r.createCell(4)
+    r4.setCellValue(o.room)
+    r4.setCellStyle(cellStyle)
+    val r5 = r.createCell(5)
+    r5.setCellValue(o.tel)
+    r5.setCellStyle(cellStyle)
   }
 
   def set(r: Row, a: Array[String]): Unit = {
